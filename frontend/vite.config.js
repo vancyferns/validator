c821819179fb49cd576be66fdb8cc9770b827e03
vancyferns <vanferns2004@gmail.com>
2025-10-22 18:40:13 +0530

    made chnages for deployment

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/validator/frontend/',
+  // Add this line:
+  base: '/validator/' 
 })
